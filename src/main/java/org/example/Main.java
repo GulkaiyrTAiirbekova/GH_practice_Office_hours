952befea237a8070751a3009336bdcb1e27ff019
Your Name <taiirbekovag@gmail.com>
2025-05-05 07:50:00 +0600

    Example from variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
+package org.example;
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        System.out.printf("Hello and welcome!");
+//    }
+//}
+public class Main{
+    public static void main(String [] args){
+        int items = 56;
+        float costPerItem =8.99f;
+        float totalCost  =items * costPerItem;
+        char currency = '$';
 
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
+        System.out.println("Number of items: " + items);
+        System.out.println("Cost per item: " + costPerItem);
+        System.out.println("Total cost = " + totalCost +currency);
     }
 }
